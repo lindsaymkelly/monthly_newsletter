e8e4adba557aea260267166eb4586d540b6c5241
lindsaymkelly <lindzlady@gmail.com>
2018-03-03 10:57:34 -0500

    Finalize front end and server side validation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,29 +7,15 @@ class PagesController < ApplicationController
   def send_sign_up
     @user = User.new(user_params)
     new_email = EmailAddress.new(user: @user, email: params[:email])
-    binding.pry
     if @user.valid? && new_email.valid?
       @user.save
       new_email.save
       redirect_to recipients_path
     else
-      @errors = @user.errors.full_messages && new_email.errors.full_messages
+      new_email.valid?
+      @errors = @user.errors.full_messages + new_email.errors.full_messages
       render 'sign_up'
     end
-    # if @user.valid?
-    #   new_email = EmailAddress.new(user: @user, email: params[:email])
-    #   if new_email.valid?
-    #     @user.save
-    #     new_email.save
-    #     redirect_to recipients_path
-    #   else
-    #     @errors = @user.errors.full_messages && new_email.errors.full_messages
-    #     render 'sign_up'
-    #   end
-    # else
-    #   @errors = @user.errors.full_messages
-    #   render 'sign_up'
-    # end
   end
 
   def recipients
