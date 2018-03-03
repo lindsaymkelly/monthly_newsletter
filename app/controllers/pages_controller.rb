b49b37df4ac620b2ca9b0b637155af51d006a7e4
lindsaymkelly <lindzlady@gmail.com>
2018-03-02 19:46:50 -0500

    Create initial routes and controller methods

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+
+  def sign_up
+    @user = User.new
+  end
+
+  def send_sign_up
+    @user = User.new(user_params)
+    if @user.save
+      new_email = EmailAddress.create(user: @user, email: params[:email])
+      redirect_to recipients_path
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def recipients
+    @users = User.all
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:firstname, :lastname)
+    end
+end
